c5907a5a8662985679696a1695bca87f9d9eefbd
KOlegD <43730369+KOlegD@users.noreply.github.com>
2021-08-01 16:10:56 +0300

    12

--- app.py
+++ app.py
@@ -4,18 +4,22 @@ from flask import Flask, request, jsonify
 import pickle as p
 
 app = Flask(__name__)
-modelfile = 'knnpickle_file'
-model = p.load(open(modelfile, 'rb'))
+with open("text.txt", "r") as file:
+    str1 = str(file.read())
+print(str1)
+# modelfile = '/knnpickle_file'
+# model = p.load(open(modelfile, 'rb'))
+
 @app.route('/', methods=['POST'])
 def makecalc():
     # data = request.get_json()
     # prediction = model.predict(data)
     # flower = ['Setosa', 'Versicolor', 'Virginica']
     # prediction = flower[int(prediction)]
-    return jsonify("2")
+    return jsonify(str1)
 
 
 if __name__ == '__main__':
     # app.run(port=5000, host='127.0.0.1')
-    # port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(debug=True, host='0.0.0.0')
