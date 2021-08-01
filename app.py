54bba7787aa77e5a43664961a473e2edd6a4924b
KOlegD <43730369+KOlegD@users.noreply.github.com>
2021-08-01 14:48:40 +0300

    9

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ from flask import Flask, request, jsonify
 import pickle as p
 
 app = Flask(__name__)
-
+modelfile = 'knnpickle_file'
+model = p.load(open(modelfile, 'rb'))
 @app.route('/', methods=['POST'])
 def makecalc():
     data = request.get_json()
@@ -16,7 +17,5 @@ def makecalc():
 
 if __name__ == '__main__':
     # app.run(port=5000, host='127.0.0.1')
-    modelfile = 'knnpickle_file'
-    model = p.load(open(modelfile, 'rb'))
     # port = int(os.environ.get("PORT", 5000))
     app.run(debug=True, host='0.0.0.0')
